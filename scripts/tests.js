6afe52e34897f7006b20c0235cb809b00df8d684
JLafreniere <jonathan.lafreniere@hotmail.com>
2016-11-12 16:22:38 -0500

    Presentation 1

--- scripts/tests.js
+++ scripts/tests.js
@@ -1,8 +1,8 @@
 /*
 * @Author: JLafreniere
 * @Date:   2016-11-01 19:53:00
-* @Last Modified by:   1425165
-* @Last Modified time: 2016-11-07 15:10:02
+* @Last Modified by:   JLafreniere
+* @Last Modified time: 2016-11-12 16:21:12
 */
 
 function workshift(emp, dateDebut, dateFin){
@@ -24,20 +24,15 @@ let ws6 = new workshift("Pierre", new Date(2016, 9, 31, 4, 45, 0), new Date(2016
 let ws7 = new workshift("Jean", new Date(2016, 9, 31, 5, 45, 0), new Date(2016, 9, 4, 20, 15, 0));
 let ws8 = new workshift("Marie", new Date(2016, 9, 31, 18, 45, 0), new Date(2016, 9, 4, 23, 15, 0));
 
-if(true){
-let shifts = [ws1, ws2, ws3, ws4, ws5, ws6, ws7, ws8];
 
+let shifts = [ws1, ws2, ws3, ws4, ws5, ws6, ws7, ws8];
 afficherShifts(shifts);
-}
+
 
 
 function kek(){
 console.log("DTP VALUE: " + document.getElementById("date_sem").value)
 }
 
-
-$("#datetimepicker").on("dp.change",function (e) {
-            console.log("test");
-});
 
 DrawRect(ws1.debut, ws1.fin);
